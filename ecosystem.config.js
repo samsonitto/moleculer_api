016c71117965d650884956d8e5e19a83b94f9561
Samson Azizyan <samson.azizyan@liveto.io>
2023-09-10 09:26:06 +0300

    Updated the script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: 'my-moleculer-app',
-      script: 'path/to/your/app.js', // Replace with the correct path to your Moleculer.js script
+      name: 'moleculer_app',
+      script: 'moleculer.config.js', // Replace with the correct path to your Moleculer.js script
       instances: 1,
       autorestart: true,
       watch: false,
